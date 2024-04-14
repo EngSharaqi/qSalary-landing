25e23e61eeafd59e3414e3dfa26edac1fec08f5c
EngSharaqi <muhammadsharaqi@gmail.com>
2024-04-14 15:35:05 +0200

    add all

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+                dots: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+});
