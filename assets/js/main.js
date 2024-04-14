4df95633ea5ecf0ca11ff11b226c362641bfe548
EngSharaqi <muhammadsharaqi@gmail.com>
2024-04-14 16:33:15 +0200

    some enhancements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,45 @@
 const preloader = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", function () {
-    preloader.classList.add("loaded");
-    document.body.classList.add("loaded");
-    
-    this.setTimeout(() => {
-        preloader.remove();
-    }, 2000)
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+
+  this.setTimeout(() => {
+    preloader.remove();
+  }, 2000)
 });
 
 $(document).ready(function () {
-    $('.autoplay').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        infinite: true,
-                dots: false,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-    });
+  $('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
